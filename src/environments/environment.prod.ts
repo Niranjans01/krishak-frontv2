b769e42221d346ec0256008d1b1206899fc7b53b
Niranjans01 <nkrsubedi@gmail.com>
2019-09-25 20:18:42 +0545

    new type design and implementation

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: true,
+  firebase: {
+    // apiKey: "AIzaSyD-CtUI9e8Lu6wziOHyszCiEb-5yaVBgxY",
+    // authDomain: "eshop-angular.firebaseapp.com",
+    // databaseURL: "https://eshop-angular.firebaseio.com",
+    // projectId: "eshop-angular",
+    // storageBucket: "eshop-angular.appspot.com",
+    // messagingSenderId: "380758333479"
+    apiKey: "AIzaSyBSfnumOSf130WDRAXbG1CbTP4wLMtMKko",
+    authDomain: "krishakbros.firebaseapp.com",
+    databaseURL: "https://krishakbros.firebaseio.com",
+    projectId: "krishakbros",
+    storageBucket: "",
+    messagingSenderId: "330266835457",
+    appId: "1:330266835457:web:618e3a1aceafb049e55e00"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD-CtUI9e8Lu6wziOHyszCiEb-5yaVBgxY",
+    authDomain: "eshop-angular.firebaseapp.com",
+    databaseURL: "https://eshop-angular.firebaseio.com",
+    projectId: "eshop-angular",
+    storageBucket: "eshop-angular.appspot.com",
+    messagingSenderId: "380758333479"
+    // apiKey: "AIzaSyBSfnumOSf130WDRAXbG1CbTP4wLMtMKko",
+    // authDomain: "krishakbros.firebaseapp.com",
+    // databaseURL: "https://krishakbros.firebaseio.com",
+    // projectId: "krishakbros",
+    // storageBucket: "",
+    // messagingSenderId: "330266835457",
+    // appId: "1:330266835457:web:618e3a1aceafb049e55e00"
+  }
+};

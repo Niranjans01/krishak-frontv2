795e5c664acf535885dae1b1141e72cf4001fae8
BishalRaj <bisal1shakya@gmail.com>
2019-10-20 20:14:27 +0545

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -49,30 +49,7 @@ export class LoginComponent implements OnInit{
       if (this.loginForm.invalid) {
           return;
       }
-    //   this.authenticationService.login(this.f.username.value, this.f.password.value)
-    //       .pipe(first())
-    //       .subscribe(
-    //           data => {
-    //               this.router.navigate([this.returnUrl]);
-    //           },
-    //           error => {
-    //               this.alertService.error(error);
-    //               this.loading = false;
-    //           });
-    // else if (!this.f.username.value.match('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')) {
-    //   $('#email-validation').css({ 'color': 'red' });
-    //   $('#email-validation').text('Please enter valid email *');
-    //   $('#email').val('')
-    //   $('#email').focus();
-    // }
-
-    // else if (this.f.password.value == '') {
-    //   $('#password-validation').css({ 'color': 'red' });
-    //   $('#password-validation').text('Please enter your password *');
-    //   $('#password').focus();
-
-    // }
-    else {
+      else {
       $.ajax({
         url: 'http://localhost:90/login',
         type: 'POST',
@@ -86,7 +63,7 @@ export class LoginComponent implements OnInit{
             window.location.href = '/admin';
             localStorage.setItem('success_login', 'session_verified');
           }
-          else if(data.success == false){
+          else{
             alert('Invalid credintials');
           }
         },
@@ -96,7 +73,6 @@ export class LoginComponent implements OnInit{
       })
     }
     this.loading = true;
-
   }
 }
 
